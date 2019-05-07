e4a59fef09de92c339338925611f36571faafd60
Mohsen Abdelkareem <mohsenAbdelkareem.thechefz>
2019-05-07 12:30:06 +0200

    // update gradle
    (cherry picked from commit 23015dbeee619aa77a62ec1343ec74e5e8d30204)

--- app/src/main/java/com/android/placepicker/ui/LocationUtils.java
+++ app/src/main/java/com/android/placepicker/ui/LocationUtils.java
@@ -33,76 +33,76 @@ public final class LocationUtils {
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
         builder.setAlwaysShow(true);
 
-        Task<LocationSettingsResponse> result =
-                LocationServices.getSettingsClient(context).checkLocationSettings(builder.build());
-
-        LocationCallback mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    locationListener.onLocationError();
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    // Update UI with location data
-                    // ...
-                    locationListener.onLocationChange(location);
-                    if (location != null){
-                        return;
-                    }
-                }
-            }
-        };
-
-
-        result.addOnCompleteListener(task -> {
-            try {
-                LocationSettingsResponse response = task.getResult(ApiException.class);
-                assert response != null;
-                response.getLocationSettingsStates();
-
-                mFusedLocationClient.requestLocationUpdates(locationRequest,
-                        mLocationCallback,
-                        null);
-
-                mFusedLocationClient.getLastLocation()
-                        .addOnSuccessListener((Activity)context, location -> {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                mFusedLocationClient.removeLocationUpdates(mLocationCallback);
-                                locationListener.onLocationChange(location);
-                            }else {
-                                locationListener.onLocationError();
-                            }
-                        });
-
-            } catch (ApiException exception) {
-                switch (exception.getStatusCode()) {
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                        // Location settings are not satisfied. But could be fixed by showing the
-                        // user a dialog.
-                        try {
-                            // Cast to a resolvable exception.
-                            ResolvableApiException resolvable = (ResolvableApiException) exception;
-                            // Show the dialog by calling startResolutionForResult(),
-                            // and check the result in onActivityResult().
-                            resolvable.startResolutionForResult(
-                                    (Activity) context,
-                                    REQUEST_CHECK_SETTINGS);
-                        } catch (IntentSender.SendIntentException | ClassCastException e) {
-                            // Ignore the error.
-                            locationListener.onLocationError();
-                        }
-                        break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        // Location settings are not satisfied. However, we have no way to fix the
-                        // settings so we won't show the dialog.
-                        locationListener.onLocationError();
-                        break;
-                }
-            }
-        }).addOnCanceledListener(locationListener::onLocationError)
-        .addOnFailureListener(e -> locationListener.onLocationError());
+//        Task<LocationSettingsResponse> result =
+//                LocationServices.getSettingsClient(context).checkLocationSettings(builder.build());
+//
+//        LocationCallback mLocationCallback = new LocationCallback() {
+//            @Override
+//            public void onLocationResult(LocationResult locationResult) {
+//                if (locationResult == null) {
+//                    locationListener.onLocationError();
+//                    return;
+//                }
+//                for (Location location : locationResult.getLocations()) {
+//                    // Update UI with location data
+//                    // ...
+//                    locationListener.onLocationChange(location);
+//                    if (location != null){
+//                        return;
+//                    }
+//                }
+//            }
+//        };
+//
+//
+//        result.addOnCompleteListener(task -> {
+//            try {
+//                LocationSettingsResponse response = task.getResult(ApiException.class);
+//                assert response != null;
+//                response.getLocationSettingsStates();
+//
+//                mFusedLocationClient.requestLocationUpdates(locationRequest,
+//                        mLocationCallback,
+//                        null);
+//
+//                mFusedLocationClient.getLastLocation()
+//                        .addOnSuccessListener((Activity)context, location -> {
+//                            // Got last known location. In some rare situations this can be null.
+//                            if (location != null) {
+//                                mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+//                                locationListener.onLocationChange(location);
+//                            }else {
+//                                locationListener.onLocationError();
+//                            }
+//                        });
+//
+//            } catch (ApiException exception) {
+//                switch (exception.getStatusCode()) {
+//                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+//                        // Location settings are not satisfied. But could be fixed by showing the
+//                        // user a dialog.
+//                        try {
+//                            // Cast to a resolvable exception.
+//                            ResolvableApiException resolvable = (ResolvableApiException) exception;
+//                            // Show the dialog by calling startResolutionForResult(),
+//                            // and check the result in onActivityResult().
+//                            resolvable.startResolutionForResult(
+//                                    (Activity) context,
+//                                    REQUEST_CHECK_SETTINGS);
+//                        } catch (IntentSender.SendIntentException | ClassCastException e) {
+//                            // Ignore the error.
+//                            locationListener.onLocationError();
+//                        }
+//                        break;
+//                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+//                        // Location settings are not satisfied. However, we have no way to fix the
+//                        // settings so we won't show the dialog.
+//                        locationListener.onLocationError();
+//                        break;
+//                }
+//            }
+//        }).addOnCanceledListener(locationListener::onLocationError)
+//        .addOnFailureListener(e -> locationListener.onLocationError());
     }
 
     public static String[] getAddressFromLocation(Context context , Location location){
